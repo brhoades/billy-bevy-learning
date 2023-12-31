5b114018f133c102f6a5e1379b676ad24b42046f
Billy J Rhoades II <me@brod.es>
2023-12-31 12:49:27 -0800

    flake: add darwin support

--- flake.nix
+++ flake.nix
@@ -6,8 +6,11 @@
   outputs = { self, nixpkgs, flake-utils }: let
     pkgsFor = system: import nixpkgs {
       inherit system;
-    }; in (flake-utils.lib.eachDefaultSystem (system: {
-      devShells.default = with (pkgsFor system); mkShell {
+    }; in (flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = pkgsFor system;
+      isLinux = with pkgs; lib.strings.hasInfix "linux" system;
+    in {
+      devShells.default = with pkgs; mkShell {
         buildInputs = [
           cargo
           rustc
@@ -17,18 +20,23 @@
           bacon
 
           pkg-config
+        ] ++ (lib.optionals pkgs.stdenv.isLinux [
           alsaLib
           udev
           xorg.libX11
-        ];
+        ])
+        ++ (lib.optionals pkgs.stdenv.isDarwin (with darwin.apple_sdk; [
+          libiconv
+          frameworks.AppKit
+        ]));
 
-        LD_LIBRARY_PATH = lib.makeLibraryPath (with xorg; [
+        LD_LIBRARY_PATH = lib.optional pkgs.stdenv.isLinux (lib.makeLibraryPath (with xorg; [
           libX11
           libXcursor
           libXrandr
           libXi
           vulkan-loader
-        ]);
+        ]));
       };
     }));
 }
